cdb0663c4c52e15a87f3f1f220320d6bae5459a9
Jonathan Chaffer <jonathanchaffer@gmail.com>
2024-08-30 10:45:35 -0400

    Add comments to eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,12 @@ export default [
     files: ["**/*.{js,mjs,cjs,ts}"],
     languageOptions: { globals: globals.node },
     rules: {
+      // Disallow relative imports from parent directories (e.g. `../foo`).
+      // Relative imports from siblings (e.g. `./foo`) are still allowed.
       "no-restricted-imports": ["error", { patterns: ["..*"] }],
+
+      // Disallow unused variables, except for variables prefixed with an
+      // underscore.
       "@typescript-eslint/no-unused-vars": [
         "warn",
         {
