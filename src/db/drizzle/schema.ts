104390550c700dd1b2c3d69c82e40886de12140f
Jonathan Chaffer <jonathanchaffer@gmail.com>
2024-08-30 12:44:12 -0400

    make user first/last name non-nullable

--- src/db/drizzle/schema.ts
+++ src/db/drizzle/schema.ts
@@ -2,6 +2,6 @@ import { pgTable, text, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").notNull().primaryKey().defaultRandom(),
-  firstName: text("first_name"),
-  lastName: text("last_name"),
+  firstName: text("first_name").notNull(),
+  lastName: text("last_name").notNull(),
 });
